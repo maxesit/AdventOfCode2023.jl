e236d57272cd9ee70e3d7530e58fb69171c7e13d
maxesit <mpolic2000@gmail.com>
2023-12-21 00:22:09 +0100

    Update

--- src/day02.jl
+++ src/day02.jl
@@ -1,21 +1,21 @@
-module aoc02
+module Day02
 
 using AdventOfCode2023
-#include(".\\AdventOfCode2023.jl")
+    # BIG THANKS TO "github.com/goggle", using his way  of reading input made everything so much easier
+    # BIG THANKS TO "github.com/goggle", using his way  of reading input made everything so much easier
 
- # BIG THANKS TO "github.com/goggle", using his way  of reading input made everything so much easier
 
+function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02.txt")))
+    data = parse_input(input)
+    p2 = sum(prod(maximum(x -> x[i], v) for i ∈ 1:3) for v ∈ values(data))
+    return [part1(data), p2]
+end
 
-function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02.txt"))) 
-#    data = parseInput(input);
-#    return nothing;
-end;
-
-function parseInput(input)
-    data = Dict{Int,Vector{Tuple{Int,Int,Int}}}()
-    for line ∈ split(rstrip(input),"\n")
-        game,gameData = split(line,:)
-        gameId = parse(Int, split(game, " ")[2])
+function parse_input(input)
+    data = Dict{Int, Vector{Tuple{Int,Int,Int}}}()
+    for line ∈ split(rstrip(input), "\n")
+        game, gdata = split(line, ":")
+        gameid = parse(Int, split(game, " ")[2])
         v = Vector{Tuple{Int,Int,Int}}()
         for round ∈ split(gdata, ";")
             r = g = b = 0
@@ -36,10 +36,20 @@ function parseInput(input)
     end
     return data
 end
+
 function part1(data::Dict{Int, Vector{Tuple{Int,Int,Int}}})
-    return 1;
-end;
+    s = sum(keys(data))
+    rmax, gmax, bmax = 12, 13, 14
+    for (k, v) ∈ data
+        for (r, g, b) ∈ v
+            if r > rmax || g > gmax || b > bmax
+                s -= k
+                break
+            end
+        end
+    end
+    return s
+end
 
 
-
-end; # module aoc02
+end # module
