a647453076110cea2b11bd12693555e764804559
maxesit <mpolic2000@gmail.com>
2023-12-21 01:30:28 +0100

    Did Tasks on my own this time :D

--- src/day02.jl
+++ src/day02.jl
@@ -1,55 +1,62 @@
-module Day02
-
 using AdventOfCode2023
+
     # BIG THANKS TO "github.com/goggle", using his way  of reading input made everything so much easier
     # BIG THANKS TO "github.com/goggle", using his way  of reading input made everything so much easier
+    # I used file structure and parseInput from him, tried to do t1 and t2 on my own.
+    # I hope to do code every other thing in this format too :D
 
+function day02(input::String = readInput(joinpath(@__DIR__,"..","data","day02.txt")))
+        data = parseInput(input);
+        t1 = Task1(data);
+        t2 = sum(prod(maximum(x -> x[i], values(data[j])) for i in 1:3) for j in keys(data))  #[7 16 4]
+        return t1,t2;
+end;
 
-function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02.txt")))
-    data = parse_input(input)
-    p2 = sum(prod(maximum(x -> x[i], v) for i ∈ 1:3) for v ∈ values(data))
-    return [part1(data), p2]
-end
-
-function parse_input(input)
-    data = Dict{Int, Vector{Tuple{Int,Int,Int}}}()
-    for line ∈ split(rstrip(input), "\n")
+function parseInput(input)
+    data = Dict{Int,Vector{Tuple{Int,Int,Int}}}()
+    for line ∈ split(rstrip(input),"\n")
         game, gdata = split(line, ":")
-        gameid = parse(Int, split(game, " ")[2])
+        gameid = parse(Int, split(game," ")[2])
         v = Vector{Tuple{Int,Int,Int}}()
-        for round ∈ split(gdata, ";")
-            r = g = b = 0
+        for round ∈ split(gdata,";")
+            r = g = b = 0;
             numbers = findall(r"\d+", round)
-            colors = findall(r"red|green|blue", round)
-            for (n, c) ∈ zip(numbers, colors)
-                if round[c] == "red"
-                    r = parse(Int, round[n])
-                elseif round[c] == "green"
-                    g = parse(Int, round[n])
+            colors  = findall(r"red|blue|green", round)
+            for (n, c) ∈ zip(numbers,colors)
+                if     round[c] == "red"
+                    r = parse(Int,round[n])
                 elseif round[c] == "blue"
-                    b = parse(Int, round[n])
-                end
-            end
-            push!(v, (r, g, b))
-        end
-        data[gameid] = v
-    end
-    return data
-end
+                    b = parse(Int,round[n])
+                elseif round[c] == "green"
+                    g = parse(Int,round[n])
+                end;
+            end;
+            push!(v,(r,g,b))
+        end;
+        data[gameid] = v;
+    end;
+    return data;
+end;
+#length(data)
+function Task1(data::Dict{Int,Vector{Tuple{Int,Int,Int}}})
+    idsum = 0;
+    r_max = 12;
+    g_max = 13;
+    b_max = 14;
+    for (k,v) ∈ data
+        flag = true;
+        for (r,g,b) ∈ v
+            if (( r > r_max )||( b > b_max )||( g > g_max ))
+                flag = false;
+                break;
+            end;
+        end;
+        if flag == true
+            idsum += k;
+        end;
+    end;
+    return idsum;
+end; # module day02
 
-function part1(data::Dict{Int, Vector{Tuple{Int,Int,Int}}})
-    s = sum(keys(data))
-    rmax, gmax, bmax = 12, 13, 14
-    for (k, v) ∈ data
-        for (r, g, b) ∈ v
-            if r > rmax || g > gmax || b > bmax
-                s -= k
-                break
-            end
-        end
-    end
-    return s
-end
-
-
-end # module
+# testa = (maximum(x -> x[i], values(data[1])) for i ∈ 1:3)
+# testb = prod((maximum(x -> x[i], values(data[j])) for i ∈ 1:3) for j ∈ keys(data))
